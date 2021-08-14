2254cec4423d30be5dc4b35110977a9a51fbac80
yuewang <yuewang199511@gmail.com>
2021-08-14 19:33:06 +0800

    set up debug tool bar

--- twitter/settings.py
+++ twitter/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # third party
     'rest_framework',
+    'debug_toolbar',
 
     # project apps
     'accounts'
@@ -57,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter.urls'
@@ -132,3 +134,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+INTERNAL_IPS = ['10.0.2.2']
