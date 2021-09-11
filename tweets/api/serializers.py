0c3649698e3933e2b44b28567cc75ff926af4439
yuewang <yuewang199511@gmail.com>
2021-09-11 16:14:08 +0800

    let tweet have comments

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,4 +1,5 @@
 from accounts.api.serializers import UserSerializer
+from comments.api.serializers import CommentSerializer
 from rest_framework import serializers
 from tweets.models import Tweet
 
@@ -23,3 +24,15 @@ class TweetCreateSerializer(serializers.ModelSerializer):
         content = validated_data['content']
         tweet = Tweet.objects.create(user=user, content=content)
         return tweet
+
+class TweetSerializerWithComments(serializers.ModelSerializer):
+    user = UserSerializer()
+    # <HOMEWORK> 使用 serialziers.SerializerMethodField 的方式实现 comments
+    # 通过指定source='comment_set'可以查找到所有该tweet对应的comment.这里会自动生成一个外键映射
+    # 所有有1对多的外键都会自动生成一个_set的属性
+    # 此处为DRF的反向关联功能，见 https://docs.djangoproject.com/zh-hans/3.2/topics/db/queries/#related-objects
+    comments = CommentSerializer(source='comment_set', many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'comments', 'created_at', 'content')
