419feb6d5b369a2e7fec82cc62d7a9dfb7f63929
yuewang <yuewang199511@gmail.com>
2021-09-14 19:54:18 +0800

    create tweet photo model

--- tweets/test.py
+++ tweets/test.py
@@ -1,7 +1,8 @@
-from testing.testcases import TestCase
 from datetime import timedelta
 from utils.time_helpers import utc_now
-
+from testing.testcases import TestCase
+from tweets.constants import TweetPhotoStatus
+from tweets.models import TweetPhoto
 
 class TweetTests(TestCase):
     def setUp(self):
@@ -23,3 +24,13 @@ class TweetTests(TestCase):
         dongxie = self.create_user('dongxie')
         self.create_like(dongxie, self.tweet)
         self.assertEqual(self.tweet.like_set.count(), 2)
+
+    def test_create_photo(self):
+        # 测试可以成功创建 photo 的数据对象
+        photo = TweetPhoto.objects.create(
+            tweet=self.tweet,
+            user=self.linghu,
+        )
+        self.assertEqual(photo.user, self.linghu)
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
