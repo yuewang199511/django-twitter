3a17acc9cba7212836128c35c9e88fda217c76ee
yuewang199511 <46737494+yuewang199511@users.noreply.github.com>
2025-08-18 15:08:13 -0700

    Update provision.sh
    change some desciptions

--- provision.sh
+++ provision.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 echo 'Start!'
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
-cd /vagrant
 sudo apt-get update
 sudo apt-get install tree
-# 安装配置mysql8
+#  install mysql8
 if ! [ -e /vagrant/mysql-apt-config_0.8.15-1_all.deb ]; then
  wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
 fi
@@ -16,20 +15,16 @@ if [ ! -f "/usr/bin/pip" ]; then
  sudo apt-get install -y python-setuptools
  sudo ln -s /usr/bin/pip3 /usr/bin/pip
 else
- echo "pip3 已安装"
+ echo "pip3 installed"
 fi
-# 升级pip，⽬前存在问题，read timed out，看脸，有时候可以，但⼤多时候不⾏
 # python -m pip install --upgrade pip
-# 换源完美解决
-# 安装pip所需依赖
+# using tsinghua resource in china
 pip install --upgrade setuptools -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install --ignore-installed wrapt -i https://pypi.tuna.tsinghua.edu.cn/simple
-# 安装pip最新版
 pip install -U pip -i https://pypi.tuna.tsinghua.edu.cn/simple
-# 根据 requirements.txt ⾥的记录安装 pip package，确保所有版本之间的兼容性
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-# 设置mysql的root账户的密码为yourpassword
-# 创建名为twitter的数据库
+# setting up mysql password as yourpassword
+# setting up a database called twitter
 sudo mysql -u root << EOF
  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY
 'yourpassword';
@@ -37,19 +32,13 @@ sudo mysql -u root << EOF
  show databases;
  CREATE DATABASE IF NOT EXISTS twitter;
 EOF
-# fi
-# superuser名字
+# setup up super user
 USER="admin"
-# superuser密码
 PASS="admin"
-# superuser邮箱
 MAIL="admin@twitter.com"
+
+# test the project
 script="
-断开虚拟机连接，重新执⾏脚本：
-5. 运⾏项⽬
-在虚拟机上运⾏项⽬
-我们重新连接虚拟机，进⼊ /vagrant ⽬录，并执⾏如下命令，运⾏我们的项⽬：
-如果运⾏成功，我们可以看到如下画⾯
 from django.contrib.auth.models import User;
 username = '$USER';
 password = '$PASS';
@@ -61,10 +50,4 @@ else:
  print('Superuser creation skipped.');
 "
 printf "$script" | python manage.py shell
-# 如果想直接进⼊/vagrant路径下
-# 请输⼊vagrant ssh命令进⼊
-# ⼿动输⼊
-# 输⼊ls -a
-# 输⼊ vi .bashrc
-# 在最下⾯，添加cd /vagrant
 echo 'All Done!'
